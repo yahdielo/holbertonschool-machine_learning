64587c52fda2c036debcbd4aa49df94ea846341b
yahdielo <yahdielaz@gmail.com>
2023-07-24 14:02:11 -0400

    getting shape of a metrix

---
+++ math/linear_algebra/2-size_me_please.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+    This module calculates the shape of a matrix,
+    calculating the rowns, columns and matrixes.
+    The formula use to calculate rows is len(matrix),
+    and to get the columns is len(matrix[0])
+"""
+def matrix_shape(matrix):
+    shape = []
+    while isinstance(matrix, list):
+        shape.append(len(matrix))
+        matrix = matrix[0] 
+    return shape
