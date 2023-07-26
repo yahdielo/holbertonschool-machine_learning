afba7908812fb53a97dd6fc9cc725bae34ea927a
yahdielo <yahdielaz@gmail.com>
2023-07-26 16:41:05 -0400

    concat 2 arrays

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+""" this modules concatenates thwo arrays"""
+
+
+def cat_arrays(arr1, arr2):
+
+    """ this modules concatenates thwo arrays"""
+
+    if arr1 and arr2:
+        newList = []
+        newList.append(arr1 + arr2)
+        return newList
+    return None
