8f9464f4fb15d8315c7e8f25589f3682dd66dd45
yahdielo <yahdielaz@gmail.com>
2023-07-26 13:17:03 -0400

    transponse matrix fix, index out of range error

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -6,22 +6,23 @@
 def matrix_transpose(matrix):
     """ This modules transverse a 2D matrix
         creating a new matrix with nested list comprehension,
-        creating a matrix with the same dimensions and 
+        creating a matrix with the same dimensions and
         adding the transverse values to the newmatrix
+        Nested List comprehenssion explanation:
+        nested list comprehension
+        declares 0 as is the value will initialize each space
+        in this new matrix, for _ as i dont want to run inside the array
+        in range of rows
+        and a nested loop to run in the columns of the matrix
     """
 
     rows = len(matrix)
     col = len(matrix[0])
 
-    #nested list comprehension
-    #declares 0 as is the value will initialize each/
-    #// space in this new matrix//
-    #for _ as i dont want to run inside the array in range of rows
-    #// and a nested loop to run in the columns of the matrix
     newMatrix = [[0 for _ in range(rows)] for _ in range(col)]
 
     for i in range(rows):
         for j in range(col):
             newMatrix[j][i] = matrix[i][j]
-            
+
     return newMatrix
