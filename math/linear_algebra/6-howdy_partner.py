c57d044ffa69a02b06887e90e488ac82e5044b40
yahdielo <yahdielaz@gmail.com>
2023-07-26 16:46:00 -0400

    concat 2 arrays

--- math/linear_algebra/6-howdy_partner.py
+++ math/linear_algebra/6-howdy_partner.py
@@ -9,6 +9,6 @@ def cat_arrays(arr1, arr2):
 
     if arr1 and arr2:
         newList = []
-        newList.append(arr1 + arr2)
-        return list(newList)
+        newList.append(list(arr1) + list(arr2))
+        return newList
     return None
