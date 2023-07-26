79807a4d969025aaddc6cc41a49d314b09f6cba6
yahdielo <yahdielaz@gmail.com>
2023-07-26 16:25:22 -0400

    handle error if matrixes are empty

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -7,17 +7,17 @@ def add_matrices2D(mat1, mat2):
     """ This modules sums two matrixes together """
 
 
-    if (bool(mat1[0]) == True) and bool(mat2[0]) == True:
-        M1Shape = (len(mat1), len(mat1[0]))
-        M2Shape = (len(mat2), len(mat2[0]))
+    # if (bool(mat1[0]) == True) and bool(mat2[0]) == True:
+    M1Shape = (len(mat1), len(mat1[0]))
+    M2Shape = (len(mat2), len(mat2[0]))
 
-        if M1Shape == M2Shape:
-            newMatrix = []
-            for i in range(len(mat1)):
-                sum = []
-                for j in range(len(mat1[0])):
-                    sum.append(mat1[i][j] + mat2[i][j])
-                newMatrix.append(sum)
-                return newMatrix
-        else:
-            return None
+    newMatrix = []
+    if M1Shape == M2Shape:
+        for i in range(len(mat1)):
+            sum = []
+            for j in range(len(mat1[0])):
+                sum.append(mat1[i][j] + mat2[i][j])
+            newMatrix.append(sum)
+            return newMatrix
+    else:
+        return None
