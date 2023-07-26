e331fa99bb7d66b6d90542d3ddc40bd1fe2c67ff
yahdielo <yahdielaz@gmail.com>
2023-07-26 15:44:52 -0400

    handle error if matrixes are empty

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -9,9 +9,9 @@ def add_matrices2D(mat1, mat2):
     M1Shape = (len(mat1), len(mat1[0]))
     M2Shape = (len(mat2), len(mat2[0]))
 
-    if M1Shape == M2Shape:
-        newMatrix = [[mat1[i][j] + mat2[i][j] for i in range(len(mat1))]
-                     for j in range(len(mat1[0]))]
-        return newMatrix
-
+    if bool(mat1[0]) == True and bool(mat2[0]) == True:
+        if M1Shape == M2Shape:
+            newMatrix = [[mat1[i][j] + mat2[i][j] for i in range(len(mat1))] 
+                        for j in range(len(mat1[0]))]
+            return newMatrix
     return None
