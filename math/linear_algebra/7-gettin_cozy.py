63c6542327597e4f028cf7c29c067baa8e36b9f6
yahdielo <yahdielaz@gmail.com>
2023-07-27 13:54:27 -0400

    adding 2d matrices

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -9,7 +9,8 @@ def cat_matrices2D(mat1, mat2, axis=0):
     newmatrix = []
     if mat1[0] and mat2[0]:
         if axis == 0:
-            newmatrix = mat1 + mat2 
+            newmatrix = mat1 + mat2
+            newmatrix = newmatrix.copy()
             return newmatrix
         else:
             for i in range(len(mat1)):
