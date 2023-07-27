89969e2678c623f30a21ba075d0e3d7363db90cf
yahdielo <yahdielaz@gmail.com>
2023-07-27 09:28:55 -0400

    fixing bug

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -10,7 +10,7 @@ def add_matrices2D(mat1, mat2):
     if bool(mat1[0]) == True and bool(mat2[0]) == True:
             M1Shape = (len(mat1), len(mat1[0]))
             M2Shape = (len(mat2), len(mat2[0]))
-            if M1Shape == M2Shape:
+            if M1Shape[1] == M2Shape[1]:
                 newMatrix = []
                 newMatrix = [[mat1[i][j] + mat2[i][j] for i in range(len(mat1))] 
                             for j in range(len(mat1[0]))]
