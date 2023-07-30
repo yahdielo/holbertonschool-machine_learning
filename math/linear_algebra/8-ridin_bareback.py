44b21cc4deb499e333affa57ae9dc7b9b8178670
yahdielo <yahdielaz@gmail.com>
2023-07-29 21:28:47 -0400

    matrix multiplication

--- math/linear_algebra/8-ridin_bareback.py
+++ math/linear_algebra/8-ridin_bareback.py
@@ -16,6 +16,7 @@ def mat_mul(mat1, mat2):
 
         for i in range(len(newMatrix)):
                 for j in range(len(newMatrix[0])):
-                        newMatrix[i][j] = sum(mat1[i][k] * mat2[k][j] for k in range(len(mat1[0])))
+                        newMatrix[i][j] = sum(mat1[i][k] * mat2[k][j] 
+                                              for k in range(len(mat1[0])))
 
         return newMatrix
