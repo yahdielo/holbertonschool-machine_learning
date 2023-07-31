42167a657451c3ca537ba8bd22b557386a1e8f19
yahdielo <yahdielaz@gmail.com>
2023-07-30 22:29:08 -0400

    return shape of a matrix

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+
+""" This module returns ghe hape of a nparray """
+
+import numpy as np
+
+
+def np_shape(matrix):
+    """ This module returns ghe hape of a nparray """
+
+    matrix = np.array(matrix)
+    return matrix.shape
