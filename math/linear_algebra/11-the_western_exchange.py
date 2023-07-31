50ea2aadc99b1dfaaf972f9d4beb67b303bb8150
yahdielo <yahdielaz@gmail.com>
2023-07-30 23:05:58 -0400

    function to trnaspose a matrix using nparray method

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+""" This mdodules returns a transponse matrix using np.transponse"""
+
+
+def np_transpose(matrix):
+    """ This mdodules returns a transponse matrix using np.transponse"""
+    return matrix.transpose()
