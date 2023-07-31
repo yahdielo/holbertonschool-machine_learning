1617d63af6fb98f03d05757aacdb68741e3c46ed
yahdielo <yahdielaz@gmail.com>
2023-07-30 22:59:50 -0400

    returns the shape of a matrix or a multy dimensional array

--- math/linear_algebra/10-ill_use_my_scale.py
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -1,13 +1,9 @@
 #!/usr/bin/env python3
 
 
-""" This module returns ghe hape of a nparray """
-
-import numpy as np
+""" This module returns the shape of a nparray """
 
 
 def np_shape(matrix):
-    """ This module returns ghe hape of a nparray """
-
-    matrix = np.array(matrix)
+    """ This module returns the shape of a nparray """
     return matrix.shape
