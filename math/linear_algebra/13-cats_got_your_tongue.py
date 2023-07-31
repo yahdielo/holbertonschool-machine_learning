0801b9edf730e24c479809cdfd4a431235e69153
yahdielo <yahdielaz@gmail.com>
2023-07-30 23:30:18 -0400

    concat 2 matrixes using np.concatenate

--- math/linear_algebra/13-cats_got_your_tongue.py
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -3,6 +3,9 @@ import numpy as np
 
 """concatenates two matrices along a specific axis:"""
 
+
 def np_cat(mat1, mat2, axis=0):
-    """concatenates two matrices along a specific axis:"""
+    """ concatenates two matrices along a specific axis: using
+        np.concat and returning a new nparray
+    """
     return np.concatenate((mat1, mat2), axis=axis)
