82f8977f633a138dbca27a144df94f524c8b104b
yahdielo <yahdielaz@gmail.com>
2023-07-30 23:34:50 -0400

    matrix mulplication usinf np.matmul()

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+
+""" This module performs matrix multiplication"""
+
+
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """ This module performs matrix multiplication"""
+    return np.matmul(mat1, mat2)
