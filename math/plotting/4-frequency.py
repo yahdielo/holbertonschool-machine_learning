d630a8f7424f7ec270ffa0ca8c3e58402c835167
yahdielo <yahdielaz@gmail.com>
2023-08-01 13:26:55 -0400

    histogram of students grades

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -5,11 +5,13 @@ import matplotlib.pyplot as plt
 np.random.seed(5)
 student_grades = np.random.normal(68, 15, 50)
 
-fig, ax = plt.subplots()
-bar_width = 1
-y = np.arange(0, len(student_grades))
-
 plt.hist(student_grades, bins=range(0, 101, 10), edgecolor='black')
 plt.xlabel("Grades")
 plt.ylabel("Number of Students")
+plt.title("Project A")
+plt.margins(x=0)
+plt.xticks([0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100])
+plt.yticks([0, 5, 10, 15, 20, 25, 30])
+plt.ylim(0)
+plt.xlim(0)
 plt.show()
