9c57599dddbf97cd55c8b27b8b4f150d9d41e86f
yahdielo <yahdielaz@gmail.com>
2023-08-06 21:46:08 -0400

    fix name

---
+++ math/calculus/9-sum_total.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+
+""" This module retuns the sigma sum"""
+
+def summation_i_squared(n):
+    """ This module retuns the sigma sum"""
+
+    zigma = list(range(1, n + 1))
+
+    return sum(zigma)
